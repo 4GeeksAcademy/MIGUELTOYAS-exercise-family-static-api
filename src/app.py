cba84a74c88f71be75e610504adb03b51f3f683e
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-08-07 19:32:04 +0000

    Empezando el ejercicio

--- src/app.py
+++ src/app.py
@@ -6,14 +6,24 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
+
+#from models import Person
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+
+# initial members:
+initial_members = [
+    {
+        "name": "John",
+        "last_name": 
+
+    }
+]
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -37,6 +47,36 @@ def handle_hello():
 
 
     return jsonify(response_body), 200
+
+class FamilyStructure:
+    def __init__(self, last_name):
+        self.last_name = last_name
+        self._next_id = 1
+        self._members = []
+
+    # Este método genera un 'id' único al agregar miembros a la lista (no debes modificar esta función)
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
+
+    def add_member(self, member):
+        ## Debes implementar este método
+        ## Agrega un nuevo miembro a la lista de _members
+        pass
+
+    def delete_member(self, id):
+        ## Debes implementar este método
+        ## Recorre la lista y elimina el miembro con el id proporcionado
+        pass
+
+    def get_member(self, id):
+        ## Debes implementar este método
+        ## Recorre la lista y obtén el miembro con el id proporcionado
+        pass
+
+    def get_all_members(self, id):
+        return self._members
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
